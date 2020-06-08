3846ac43631e09d45b09b93ebf46975af8a03a07
draelios <acantefo7@gmail.com>
2020-06-08 21:33:25 +0200

    phonebook useEffect data retrieve + initial dataforcountries

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,19 +1,26 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Agenda from './components/Agenda.jsx'
 import NewPerson from './components/NewPerson.jsx'
 import Filter from './components/Filter.jsx'
+import axios from 'axios';
 
 const App = () => {
-  const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ]) ;
+
+  const getPersons = () =>{
+    axios
+    .get('http://localhost:3001/persons')
+    .then(response => {
+      setPersons(response.data);
+      setShowList(response.data);
+    })
+  }
+
+  const [ persons, setPersons ] = useState([]) ;
   const [ newName, setNewName ] = useState('');
   const [ newNumber, setNewNumber ] = useState('');
   const [ showList, setShowList ] = useState(persons);
 
+  useEffect(getPersons, []);
 
   const addPerson = (event) => {
     event.preventDefault();
