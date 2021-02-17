1f174792c119c458c8601487565f991fba08be84
draelios <acantefo7@gmail.com>
2021-02-17 23:48:45 +0100

    redux unicafe & redux anecdotes

--- part6/unicafe-redux/src/reducer.js
+++ part6/unicafe-redux/src/reducer.js
@@ -1,42 +1,40 @@
 const initialState = {
   good: 0,
   ok: 0,
-  bad: 0
-}
+  bad: 0,
+};
 
 const counterReducer = (state = initialState, action) => {
-  console.log(action)
   switch (action.type) {
     case 'GOOD': {
-      const good = state.good;
+      const { good } = state;
       const newState = {
         ...state,
-        good: good + 1
-      }
-      return newState
+        good: good + 1,
+      };
+      return newState;
     }
-      
-    case 'OK':{
-      const ok = state.ok;
+
+    case 'OK': {
+      const { ok } = state;
       const newState = {
         ...state,
-        ok: ok + 1
-      }
-      return newState
+        ok: ok + 1,
+      };
+      return newState;
     }
-    case 'BAD':{
-      const bad = state.bad;
+    case 'BAD': {
+      const { bad } = state;
       const newState = {
         ...state,
-        bad: bad + 1
-      }
-      return newState
+        bad: bad + 1,
+      };
+      return newState;
     }
     case 'ZERO':
-      return initialState
-    default: return state
+      return initialState;
+    default: return state;
   }
-  
-}
+};
 
-export default counterReducer
+export default counterReducer;
